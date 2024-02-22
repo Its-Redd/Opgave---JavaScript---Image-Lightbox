ab75acde3dfe059aff572d0d25b97f902e095a4a
Redd <piihlsonyt@gmail.com>
2024-02-22 10:45:07 +0100

    Add flexbox layout and responsive styling to gallery

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+let gallery = document.querySelector('#gallery');
+
+gallery.addEventListener('click', function (event) {
+  let target = event.target;
+  if (target.tagName === 'IMG') {
+    let overlay = document.createElement('div');
+    overlay.id = 'overlay';
+    overlay.addEventListener('click', function () {
+      if (overlay) {
+        overlay.remove();
+      }
+    });
+    let image = document.createElement('img');
+    let source1 = target.src;
+    let temp = source1.split('_');
+    image.src = './img/img/' + temp[1];
+
+    image.style.border = '3px solid white';
+    overlay.appendChild(image);
+    document.body.appendChild(overlay);
+  }
+});
